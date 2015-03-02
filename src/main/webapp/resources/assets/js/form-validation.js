b898d1ef04fa3663c0169ca9e9a7057dac163b17
c-tash <ctash23@gmail.com>
2015-03-02 20:00:39 +0300

    Registration matcher.

--- src/main/webapp/resources/assets/js/form-validation.js
+++ src/main/webapp/resources/assets/js/form-validation.js
@@ -1,45 +1,39 @@
-<script type="text/javascript">
 $(document).ready(function() {
-// Generate a simple captcha
-function randomNumber(min, max) {
-return Math.floor(Math.random() * (max - min + 1) + min);
-};
-$('#captchaOperation').html([randomNumber(1, 100), '+', randomNumber(1, 200), '='].join(' '));
- 
-$('#defaultForm').bootstrapValidator({
+
+$('#signinForm').bootstrapValidator({
 message: 'This value is not valid',
 fields: {
-username: {
-message: 'The username is not valid',
-validators: {
-notEmpty: {
-message: 'The username is required and can\'t be empty'
-},
-stringLength: {
-min: 6,
-max: 30,
-message: 'The username must be more than 6 and less than 30 characters long'
-},
-regexp: {
-regexp: /^[a-zA-Z0-9_\.]+$/,
-message: 'The username can only consist of alphabetical, number, dot and underscore'
-},
-different: {
-field: 'password',
-message: 'The username and password can\'t be the same as each other'
-}
-}
-},
-email: {
-validators: {
-notEmpty: {
-message: 'The email address is required and can\'t be empty'
-},
-emailAddress: {
-message: 'The input is not a valid email address'
-}
-}
-},
+  username: {
+    message: 'The username is not valid',
+    validators: {
+      notEmpty: {
+        message: 'The username is required and can\'t be empty'
+      },
+      stringLength: {
+        min: 6,
+        max: 30,
+        message: 'The username must be more than 6 and less than 30 characters long'
+      },
+      regexp: {
+        regexp: /^[a-zA-Z0-9_\.]+$/,
+        message: 'The username can only consist of alphabetical, number, dot and underscore'
+      },
+      different: {
+        field: 'password',
+        message: 'The username and password can\'t be the same as each other'
+      }
+    }
+  },
+  email: {
+    validators: {
+      notEmpty: {
+        message: 'The email address is required and can\'t be empty'
+      },
+      emailAddress: {
+        message: 'The input is not a valid email address'
+      }
+    }
+  },
 password: {
 validators: {
 notEmpty: {
@@ -84,4 +78,3 @@ return value == sum;
 }
 });
 });
-</script>
